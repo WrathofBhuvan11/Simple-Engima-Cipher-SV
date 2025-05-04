1b50b4ca8f6f557cd811e29f57e3c945be768449
Bhuvanesh_S_K <36232037+WrathofBhuvan11@users.noreply.github.com>
2025-05-04 22:28:15 +0530

    Update engima.sv

--- src/engima.sv
+++ src/engima.sv
@@ -9,12 +9,16 @@ module enigma (
     wire [25:0] after_rotor3_inv, after_rotor2_inv, after_rotor1_inv;
 
     plugboard pb1 (.in(input_letter), .out(after_plugboard1));
-    rotor r1_fwd (.in(after_plugboard1), .n(n1), .direction(0), .out(after_rotor1_fwd));
-    rotor r2_fwd (.in(after_rotor1_fwd), .n(n2), .direction(0), .out(after_rotor2_fwd));
-    rotor r3_fwd (.in(after_rotor2_fwd), .n(n3), .direction(0), .out(after_rotor3_fwd));
+    // Forward path through the rotors
+    rotor #(.ROTOR_TYPE(2'b00)) r1_fwd (.in(after_plugboard1), .n(n1), .direction(0), .out(after_rotor1_fwd));  // Rotor I
+    rotor #(.ROTOR_TYPE(2'b01)) r2_fwd (.in(after_rotor1_fwd), .n(n2), .direction(0), .out(after_rotor2_fwd));   // Rotor II
+    rotor #(.ROTOR_TYPE(2'b10)) r3_fwd (.in(after_rotor2_fwd), .n(n3), .direction(0), .out(after_rotor3_fwd));   // Rotor III
+    // Reflector
     reflector ref (.in(after_rotor3_fwd), .out(after_reflector));
-    rotor r3_inv (.in(after_reflector), .n(n3), .direction(1), .out(after_rotor3_inv));
-    rotor r2_inv (.in(after_rotor3_inv), .n(n2), .direction(1), .out(after_rotor2_inv));
-    rotor r1_inv (.in(after_rotor2_inv), .n(n1), .direction(1), .out(after_rotor1_inv));
+    // Inverse path through the rotors
+    rotor #(.ROTOR_TYPE(2'b10)) r3_inv (.in(after_reflector), .n(n3), .direction(1), .out(after_rotor3_inv));    // Rotor III
+    rotor #(.ROTOR_TYPE(2'b01)) r2_inv (.in(after_rotor3_inv), .n(n2), .direction(1), .out(after_rotor2_inv));   // Rotor II
+    rotor #(.ROTOR_TYPE(2'b00)) r1_inv (.in(after_rotor2_inv), .n(n1), .direction(1), .out(after_rotor1_inv));   // Rotor I
+    
     plugboard pb2 (.in(after_rotor1_inv), .out(output_letter));
 endmodule
